f27b3fef04044abf9af4283f6fc2fc29ea96bfa8
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-02 14:49:20 +0200

    trying new code

--- main.js
+++ main.js
@@ -23,28 +23,28 @@ var myArray = [
 ]
 
 
-$('#search-input').on('keyup', function(){
-    var value = $(this).val()
-    console.log('Value:', value)
-    var data = searchTable(value, myArray)
-    buildTable(data)
-})
+// $('#search-input').on('keyup', function(){
+//     var value = $(this).val()
+//     console.log('Value:', value)
+//     var data = searchTable(value, myArray)
+//     buildTable(data)
+// })
 
 buildTable(myArray)
 
-function searchTable(value, data){
-    var filteredData = []
+// function searchTable(value, data){
+//     var filteredData = []
 
-    for (var i = 0; i < data.length; i++){
-        value = value.toLowerCase()
-        var name = data[i].name.toLowerCase()
+//     for (var i = 0; i < data.length; i++){
+//         value = value.toLowerCase()
+//         var name = data[i].name.toLowerCase()
     
-        if (name.includes(value)) {
-            filteredData.push(data[i])
-        }
-    }
-    return filteredData
-} 
+//         if (name.includes(value)) {
+//             filteredData.push(data[i])
+//         }
+//     }
+//     return filteredData
+// } 
 
 
 function buildTable(data) {
