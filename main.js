f759086e58884760386165ee96bded526ea292ed
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-02-02 15:14:42 +0200

    starting with the functionality

--- main.js
+++ main.js
@@ -41,7 +41,15 @@ function buildTable(data) {
 
 
 $('th').on('click', function(){
+    var column = $(this).data('column')
+    var order = $(this).data('order')
     console.log('Column was clicked')
+
+    if (order == 'desc') {
+        
+    } else {
+
+    }
 })
 
 function myFunction() {
