e1105b7de8e86bbc5aef87d44d549ef75f87443b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-12 00:36:11 +0200

    stats

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var myArray = [
         'name' : 'Rynell', 'goals' : '5', 'assists' : '0', 'position' : 'ST'
     }, 
     {
-        'name' : 'Tumi', 'goals' : '1', 'assists' : '1', 'position' : 'MID'
+        'name' : 'Tumi', 'goals' : '1', 'assists' : '2', 'position' : 'MID'
     }, 
     {
         'name' : 'Basla', 'goals' : '0', 'assists' : '1', 'position' : 'MID'
