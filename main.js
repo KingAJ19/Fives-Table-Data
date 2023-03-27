519adf7246aef79759dd993db33673c5ba2a1359
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-27 02:33:28 +0200

    rynell

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
 var myArray = [
     {
-        'name' : 'Calvin', 'goals' : '3', 'assists' : '3', 'position' : 'GK'
-    }, 
-    {
-        'name' : 'Elan', 'goals' : '0', 'assists' : '1', 'position' : 'ST'
-    }, 
-    {
-        'name' : 'Jesse', 'goals' : '3', 'assists' : '1', 'position' : 'MID'
-    }, 
-    {
-        'name' : 'Matthew', 'goals' : '0', 'assists' : '0', 'position' : 'DEF'
-    }, 
-    {
-        'name' : 'Rynell', 'goals' : '5', 'assists' : '0', 'position' : 'ST'
-    }, 
-    {
-        'name' : 'Tumi', 'goals' : '4', 'assists' : '2', 'position' : 'MID'
-    }, 
-    {
-        'name' : 'Basla', 'goals' : '0', 'assists' : '1', 'position' : 'MID'
-    }, 
-    {
-        'name' : 'Ethan', 'goals' : '0', 'assists' : '0', 'position' : 'RW'
+        'name': 'Calvin', 'goals': '3', 'assists': '3', 'position': 'GK'
     },
     {
-        'name' : 'Jordan', 'goals' : '6', 'assists' : '2', 'position' : 'RW'
+        'name': 'Elan', 'goals': '0', 'assists': '1', 'position': 'ST'
     },
     {
-        'name' : 'Nahum', 'goals' : '0', 'assists' : '0', 'position' : 'DEF'
+        'name': 'Jesse', 'goals': '3', 'assists': '1', 'position': 'MID'
     },
     {
-        'name' : 'Nathan', 'goals' : '0', 'assists' : '0', 'position' : 'ST'
+        'name': 'Matthew', 'goals': '0', 'assists': '0', 'position': 'DEF'
     },
     {
-        'name' : 'Latham', 'goals' : '3', 'assists' : '1', 'position' : 'RW'
+        'name': 'Rynell', 'goals': '6', 'assists': '0', 'position': 'ST'
     },
     {
-        'name' : 'Kirwan', 'goals' : '2', 'assists' : '1', 'position' : 'ST'
+        'name': 'Tumi', 'goals': '4', 'assists': '2', 'position': 'MID'
     },
     {
-        'name' : 'Cullen', 'goals' : '0', 'assists' : '2', 'position' : 'MID'
+        'name': 'Basla', 'goals': '0', 'assists': '1', 'position': 'MID'
     },
     {
-        'name' : 'Luke', 'goals' : '0', 'assists' : '0', 'position' : 'DEF'
+        'name': 'Ethan', 'goals': '0', 'assists': '0', 'position': 'RW'
     },
     {
-        'name' : 'Anele', 'goals' : '1', 'assists' : '0', 'position' : 'DEF'
+        'name': 'Jordan', 'goals': '6', 'assists': '2', 'position': 'RW'
     },
     {
-        'name' : 'Dillon', 'goals' : '0', 'assists' : '0', 'position' : 'DEF'
+        'name': 'Nahum', 'goals': '0', 'assists': '0', 'position': 'DEF'
     },
     {
-        'name' : 'Lyndon', 'goals' : '1', 'assists' : '2', 'position' : 'DEF'
+        'name': 'Nathan', 'goals': '0', 'assists': '0', 'position': 'ST'
     },
     {
-        'name' : 'Leighton', 'goals' : '4', 'assists' : '2', 'position' : 'LW'
+        'name': 'Latham', 'goals': '3', 'assists': '1', 'position': 'RW'
     },
     {
-        'name' : 'Adrian', 'goals' : '3', 'assists' : '2', 'position' : 'LW'
+        'name': 'Kirwan', 'goals': '2', 'assists': '1', 'position': 'ST'
     },
-    
+    {
+        'name': 'Cullen', 'goals': '0', 'assists': '2', 'position': 'MID'
+    },
+    {
+        'name': 'Luke', 'goals': '0', 'assists': '0', 'position': 'DEF'
+    },
+    {
+        'name': 'Anele', 'goals': '1', 'assists': '0', 'position': 'DEF'
+    },
+    {
+        'name': 'Dillon', 'goals': '0', 'assists': '0', 'position': 'DEF'
+    },
+    {
+        'name': 'Lyndon', 'goals': '1', 'assists': '2', 'position': 'DEF'
+    },
+    {
+        'name': 'Leighton', 'goals': '4', 'assists': '2', 'position': 'LW'
+    },
+    {
+        'name': 'Adrian', 'goals': '3', 'assists': '2', 'position': 'LW'
+    },
+
 ]
 
 buildTable(myArray)
@@ -68,19 +68,19 @@ function buildTable(data) {
     var table = document.getElementById('myTable')
     table.innerHTML = ''
 
-    for ( var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         var row = `<tr>
                    <td>${data[i].name}</td>
                    <td>${data[i].goals}</td>
                    <td>${data[i].assists}</td>
                    <td>${data[i].position}</td>
                    </tr>`
-                table.innerHTML += row
+        table.innerHTML += row
     }
 }
 
 
-$('th').on('click', function(){
+$('th').on('click', function () {
     var column = $(this).data('column')
     var order = $(this).data('order')
     var text = $(this).html()
@@ -88,11 +88,11 @@ $('th').on('click', function(){
 
     if (order == 'desc') {
         $(this).data('order', 'asc')
-        myArray = myArray.sort((a,b) => a[column] > b[column] ? 1 : -1)
+        myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
         text += '&#9660'
     } else {
         $(this).data('order', 'desc')
-        myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
+        myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
         text += '&#9650'
     }
     $(this).html(text)
@@ -106,17 +106,17 @@ function myFunction() {
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-  
+
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
-      }
     }
 }
