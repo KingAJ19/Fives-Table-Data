2ea312474e8e424eae9eb75c6d7502c1de4919ee
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-03-29 07:55:28 +0200

    oi

--- main.js
+++ main.js
@@ -62,11 +62,11 @@ var myArray = [
 
 ]
 
-buildTable(myArray)  
+buildTable(myArray)
 
-function buildTable(data) { 
+function buildTable(data) {
     var table = document.getElementById('myTable')
-    table.innerHTML = ''  
+    table.innerHTML = ''
 
     for (var i = 0; i < data.length; i++) {
         var row = `<tr>
